15fe79c6f7b0bc0715f7e4d91b67951aee5eaf90
unknown <animeshsharma27@gmail.com>
2023-04-07 14:12:35 +0530

    first time

---
+++ BasicCoreJava2023/src/variablesAndDataTypes/DataTypes.java
@@ -0,0 +1,16 @@
+package variablesAndDataTypes;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int age = 34;
+		String name = "Animesh";
+		boolean marritalStatus = true ;
+		double height = 5.8;
+		
+		System.out.println(age);
+	}
+
+}
